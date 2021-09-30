b31d45bb8a426e5a4db1a31e7a40ecca1f73a8d2
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-30 14:57:33 +0100

    Classwork update...

--- Olatoye/OOP/main_2.py
+++ Olatoye/OOP/main_2.py
@@ -1,3 +1,5 @@
+import json
+
 from class_task import *
 
 
@@ -14,62 +16,55 @@ def add_native():
 
 
 def add_natives(number_of_natives):
+    counter = 1
     dict_of_natives = {}
-    while number_of_natives != 0:
+    while counter <= number_of_natives:
         dict_of_natives[f"Native {number_of_natives}"] = add_native()
-        number_of_natives -= 1
+        counter += 1
     return dict_of_natives
 
 
 def add_cohort(cohort_number_):
     cohort_ = Cohort(cohort_number_, input("Enter Cohort name"))
-    number_of_natives = input("Enter number of natives")
+    number_of_natives = int(input("Enter number of natives"))
     cohort_.natives_dict[f"Cohort {cohort_number_} ({cohort_.cohort_name})"] = add_natives(number_of_natives)
     return cohort_.natives_dict
 
 
 def add_cohorts(number_of_cohorts):
+    counter = 1
     dict_of_cohorts = {}
-    while number_of_cohorts != 0:
+    while counter <= number_of_cohorts:
         cohort_number_ = input('Enter Cohort Number')
         dict_of_cohorts[f"Cohort {cohort_number_}"] = add_cohort(cohort_number_)
-        number_of_cohorts -= 1
+        counter += 1
     return dict_of_cohorts
 
 
 def add_building():
     building_ = Building(input("Enter Building name"), input("Enter Building address"))
-    number_of_cohorts = input("Enter number of cohorts")
+    number_of_cohorts = int(input("Enter number of cohorts"))
     building_.cohort_dict[f"Building {number_of_cohorts}"] = add_cohorts(number_of_cohorts)
     return building_.cohort_dict
 
 
 def add_buildings(number_of_buildings_):
-    dict_of_buildings = {}
-    while number_of_buildings_ != 0:
-        dict_of_buildings[f"Building {number_of_buildings_}"] = add_building()
-        number_of_buildings_ -= 1
-    return dict_of_buildings
+    counter = 1
+    while counter <= number_of_buildings_:
+        DataBase.general_dict[f"Building {number_of_buildings_}"] = add_building()
+        counter += 1
+    return DataBase.general_dict
 
 
-if __name__ == "__main__":
+number_of_buildings = input("Enter the number of buildings")
+database = add_buildings(number_of_buildings)
+print(database)
 
-    number_of_buildings = input("Enter number of buildings")
-    buildings = add_buildings(number_of_buildings)
 
-    for building_number in buildings.keys():
-        print(building_number)
-        for building in buildings.values():
-            for cohort_number in building.keys():
-                print(cohort_number)
-                for cohort in building.values():
-                    print(cohort)
-                    for native in cohort.values():
-                        print(native)
-
-    # for key in run.keys():
-    #     print(key)
-    #     for value in run.values():
-    #         for value_ in value.values():
-    #             for value__ in value_.values():
-    #                 print(value__, end="  ")
+with open("sample_db.txt", 'w+') as f:
+    # for line in database:
+    #     print(line)
+    #     f.write(line)
+    #     f.write('\n')
+    data = json.dumps(database)
+    f.write(data)
